de5be34ec6213576a292920fba982c3bbaa8d7b4
xzotacx <jesus.152006@gmail.com>
2021-09-18 12:25:21 -0400

    agregando host

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const http = require("http");
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || "localhost";
 
 const runServer = () => {
   const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("Neliusic is Started");
+    res.setHeader("Content-Type", "application/json");
+    res.end(
+      JSON.stringify({
+        status: res.statusCode,
+        statusMessage: "Neliusic is Started",
+      })
+    );
   });
 
-  server.listen(port, () => {
-    console.log(`Server running at port: ${port}`);
+  server.listen(port, host, () => {
+    console.log(`Server running at port: ${port} in ${host}`);
     console.log(`[Node] Neliusic started`);
   });
 };
